0c711597f879c7be9c3fcb244709de17ae22a8be
Vladimir Chekhlov <chekhlov51@gmail.com>
2022-04-03 17:09:21 +0300

    Step #2

--- HW 14/Class.py
+++ HW 14/Class.py
@@ -1,6 +1,8 @@
 import sqlite3
 from pprint import pprint as pp
 
+DB_PATH = 'netflix.db'
+
 
 class NetflixDB:
     def __init__(self, path):
@@ -9,6 +11,7 @@ class NetflixDB:
     def get_data_from_db(self, query):
         with sqlite3.connect(self.path) as connection:
             cursor = connection.cursor()
+            cursor.row_factory = sqlite3.Row
             result = cursor.execute(query).fetchall()
 
         return result
@@ -24,5 +27,20 @@ class NetflixDB:
         for item in result:
             return item
 
+    def get_elements_by_year(self, from_release_year, to_release_year):
+        result = self.get_data_from_db(query=f'''
+        SELECT title, release_year  
+        FROM netflix
+        WHERE type = 'Movie'
+        GROUP BY title, release_year
+        HAVING release_year BETWEEN "{from_release_year}" AND "{to_release_year}"
+        ORDER BY release_year
+        LIMIT 100
+        
+        ''')
+        for item in result:
+            return item
 
 
+bd = NetflixDB(DB_PATH)
+pp(bd.get_elements_by_year(2020, 2021))
--- HW 14/views.py
+++ HW 14/views.py
@@ -9,19 +9,24 @@ DB_PATH = 'netflix.db'
 
 @app.route('/')
 def index_page():
-    return " Заполните адресную строку"
+    return "Заполните адресную строку"
 
 
 @app.route('/movie/<title>', methods=['GET'])
 def page_movie(title):
     db = NetflixDB(DB_PATH)
     movie = db.get_element_by_title(title)
+    return jsonify(dict(movie))
+
+
+@app.route('/movie/<int:from_year>/to/<int:to_year>')
+def page_movie_by_years(from_year, to_year):
+    item_list = []
+    db = NetflixDB(DB_PATH)
+    movie = db.get_elements_by_year(from_year, to_year)
     for item in movie:
-        return jsonify(title=item['title'],
-                       country=item['country'],
-                       release_year=item['release_year'],
-                       genre=item['listed_in'],
-                       description=item['description'])
+        item_list.append(item)
+        return jsonify(dict(item_list))
 
 
 if __name__ == "__main__":
